9ab7b59bedf2664100e48014156c477e0988d6f1
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-01-24 10:05:28 +0100

    init

---
+++ node-data-b/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.json())
+app.use(express.static(__dirname + '/static'))
+
+const url = 'mongodb://mongodb:27017/test'
+mongoose.connect(url)
+mongoose.connection.on('open', function() {
+	console.log('Connected successfully to server')
+	app.listen(3000)
+})
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.ObjectId
+
+const todosSchema = new Schema({
+	text: String,
+	done: Boolean
+})
+
+const TodosModel = mongoose.model('Todos', todosSchema)
+
+app.get('/items', async (req, res) => {
+	const todos = await TodosModel.find().exec()
+	res.status(200).json(todos)
+});
